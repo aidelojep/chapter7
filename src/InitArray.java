08831915721ad9f8fce8256b6611f232e0b2c164
aidelojep <aidelojepeter123@gmail.com>
2021-03-30 21:57:11 +0100

    Initial commit

---
+++ src/InitArray.java
@@ -0,0 +1,12 @@
+public class InitArray {
+    public static void main(String[] args) {
+      int array [] = new int[10];
+      System.out.printf("%5s%5s%n", "Index ", "Values");
+     for (int counter=0; counter< array.length; counter++ ){
+         System.out.printf("%4d%6d%n", counter, array[counter]);
+     }
+     
+        }
+
+    }
+
